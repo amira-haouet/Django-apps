43a32240e02313f37850066b0c596bd821c783c9
amira-haouet <haouetamira@gmail.com>
2021-12-08 21:01:26 +0100

    update apps

--- student_project/student_project/settings.py
+++ student_project/student_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #add all apps here in settings
+    #add all apps here in settings.py
     'student_register'
 ]
 
