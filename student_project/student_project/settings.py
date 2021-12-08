3b829cee37bb34c2894e57ab6c74932672c07518
amira-haouet <haouetamira@gmail.com>
2021-12-08 20:27:14 +0100

    add user and password tp setting.py and change databaasename and engine

--- student_project/student_project/settings.py
+++ student_project/student_project/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'student_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #change sqlite to postgresql
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': BASE_DIR / 'db.sqlite3', => change database name 
+        'NAME': 'StudentDB',
+        # if u have password and username put it here 
+        'USER':""
+  
+        
     }
 }
 
